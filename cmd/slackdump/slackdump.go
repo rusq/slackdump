d3987cc05fa2526aefde6717b52fe061f0e2bfd6
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2021-12-07 06:14:43 +1000

    OCD'd on slackdump.go

--- cmd/slackdump/slackdump.go
+++ cmd/slackdump/slackdump.go
@@ -10,59 +10,67 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"sync"
 
 	"github.com/joho/godotenv"
 	"github.com/rusq/slackdump"
 )
 
 const (
-	outputTypeJSON    = "json"
-	outputTypeText    = "text"
-	outputTypeDefault = ""
+	outputTypeJSON = "json"
+	outputTypeText = "text"
 )
 
 var _ = godotenv.Load()
 
-// flags
-var (
-	flagChannels  = flag.Bool("c", false, "list channels/conversations and their IDs for export.  Use -ct to\nspecify channel types.")
-	flagUsers     = flag.Bool("u", false, "list Users and their IDs for export. ")
-	flagDumpFiles = flag.Bool("f", false, "Dump files embedded in the conversation")
-	outputFile    = flag.String("o", "-", "output `filename` for users and channels.  Use '-' for standard\noutput.")
-	outputType    = flag.String("r", outputTypeDefault, "report `format`.  One of 'json' or 'text'")
-	tokenID       = flag.String("t", os.Getenv("SLACK_TOKEN"), "Specify slack `API_token`, get it here:\nhttps://api.slack.com/custom-integrations/legacy-tokens\n"+
-		"It is also possible to define SLACK_TOKEN environment variable.")
-	cookie = flag.String("cookie", os.Getenv("COOKIE"), "d= cookie value")
-)
+type params struct {
+	creds slackCreds
+	list  listFlags
+
+	output output
+
+	channelsToExport []string
+	dumpFiles        bool
+}
+
+type output struct {
+	filename string
+	format   string
+}
+
+type slackCreds struct {
+	token  string
+	cookie string
+}
+
+func (c slackCreds) valid() bool {
+	return c.token != "" && c.cookie != ""
+}
+
+type listFlags struct {
+	users    bool
+	channels bool
+}
+
+func (lf listFlags) present() bool {
+	return lf.users || lf.channels
+}
 
 func init() {
-	flag.Usage = usage
-}
-
-func usage() {
-	fmt.Fprintf(
-		flag.CommandLine.Output(),
-		"Slackdump dumps messages and files from slack using the provided api token.\n"+
-			"Will create a number of files having the channel_id as a name.\n"+
-			"Files are downloaded into a respective folder with channel_id\n\n"+
-			"Usage: %s [flags] [channel_id1 ... channel_idN]\n",
-		os.Args[0])
-	flag.PrintDefaults()
-}
-
-func getOutputHandle(filename string) (f io.WriteCloser, err error) {
-	if filename == "-" {
-		f = os.Stdout
-		return
+	flag.Usage = func() {
+		fmt.Fprintf(
+			flag.CommandLine.Output(),
+			"Slackdump dumps messages and files from slack using the provided api token.\n"+
+				"Will create a number of files having the channel_id as a name.\n"+
+				"Files are downloaded into a respective folder with channel_id\n\n"+
+				"Usage: %s [flags] [channel_id1 ... channel_idN]\n",
+			os.Args[0])
+		flag.PrintDefaults()
 	}
-	return os.Create(filename)
 }
 
 func main() {
-	flag.Parse()
-
-	if err := checkParameters(); err != nil {
+	params, err := checkParameters()
+	if err != nil {
 		flag.Usage()
 		log.Fatal(err)
 	}
@@ -70,24 +78,18 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	if err := run(ctx); err != nil {
+	if err := run(ctx, params); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run(ctx context.Context) error {
-	if *flagChannels || *flagUsers {
-		output, err := getOutputHandle(*outputFile)
-		if err != nil {
+func run(ctx context.Context, p params) error {
+	if p.list.channels || p.list.users {
+		if err := listEntities(ctx, p.output, p.creds, p.list); err != nil {
 			return err
 		}
-		defer output.Close()
-
-		if err = fetchData(ctx, output, *outputType); err != nil {
-			return err
-		}
-	} else if len(flag.Args()) > 0 {
-		n, err := dumpChannels(context.Background(), flag.Args())
+	} else if len(p.channelsToExport) > 0 {
+		n, err := dumpChannels(ctx, p.creds, p.channelsToExport, p.dumpFiles, p.output.format == outputTypeText)
 		if err != nil {
 			return err
 		}
@@ -98,37 +100,66 @@ func run(ctx context.Context) error {
 	return nil
 }
 
-func checkParameters() error {
-	if *tokenID == "" {
-		return fmt.Errorf("slack token not specified")
+func createFile(filename string) (f io.WriteCloser, err error) {
+	if filename == "-" {
+		f = os.Stdout
+		return
+	}
+	return os.Create(filename)
+}
+
+func checkParameters() (params, error) {
+	var p params
+	// flags
+	{
+		flag.BoolVar(&p.list.channels, "c", false, "list channels (aka conversations) and their IDs for export.")
+		flag.BoolVar(&p.list.users, "u", false, "list users and their IDs. ")
+		flag.BoolVar(&p.dumpFiles, "f", false, "enable files download")
+		flag.StringVar(&p.output.filename, "o", "-", "output `filename` for users and channels.  Use '-' for standard\noutput.")
+		flag.StringVar(&p.output.format, "r", "", "report `format`.  One of 'json' or 'text'")
+		flag.StringVar(&p.creds.token, "t", os.Getenv("SLACK_TOKEN"), "Specify slack `API_token`, get it here:\nhttps://api.slack.com/custom-integrations/legacy-tokens\n"+
+			"It is also possible to define SLACK_TOKEN environment variable.")
+		flag.StringVar(&p.creds.cookie, "cookie", os.Getenv("COOKIE"), "d= cookie value")
+		flag.Parse()
+
+		p.channelsToExport = flag.Args()
+	}
+
+	if !p.creds.valid() {
+		return p, fmt.Errorf("slack token or cookie not specified")
 	}
 	os.Unsetenv("SLACK_TOKEN")
 
-	if *outputType != "" && !(*outputType == outputTypeJSON ||
-		*outputType == outputTypeText) {
-		return fmt.Errorf("invalid output type, must use one of %s or %s", outputTypeJSON, outputTypeText)
+	if len(p.channelsToExport) == 0 && !p.list.present() {
+		return p, fmt.Errorf("no list flags specified and no channels to export")
 	}
 
-	// channels and users will have a text output (if not specified otherwise)
-	if *outputType == outputTypeDefault {
-		if *flagChannels || *flagUsers {
-			*outputType = outputTypeText
+	if p.output.format != "" && (p.output.format == outputTypeJSON ||
+		p.output.format == outputTypeText) {
+		return p, fmt.Errorf("invalid output type: %q, must use one of %v", p.output.format, []string{outputTypeJSON, outputTypeText})
+	}
+
+	// channels and users listings will be in the text format (if not specified otherwise)
+	if p.output.format == "" {
+		if p.list.present() {
+			p.output.format = outputTypeText
 		} else {
-			*outputType = outputTypeJSON
+			p.output.format = outputTypeJSON
 		}
 	}
 
-	if len(flag.Args()) == 0 && !(*flagChannels || *flagUsers) {
-		usage()
-		return fmt.Errorf("no flags specified and no channels for export")
-	}
-
-	return nil
+	return p, nil
 }
 
-func fetchData(ctx context.Context, output io.Writer, outputType string) error {
+func listEntities(ctx context.Context, output output, creds slackCreds, list listFlags) error {
+	w, err := createFile(output.filename)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
 	log.Print("initializing...")
-	sd, err := slackdump.New(ctx, *tokenID, *cookie)
+	sd, err := slackdump.New(ctx, creds.token, creds.cookie)
 	if err != nil {
 		return err
 	}
@@ -137,84 +168,82 @@ func fetchData(ctx context.Context, output io.Writer, outputType string) error {
 
 	var rep slackdump.Reporter
 	switch {
-	case *flagChannels:
+	case list.channels:
 		rep, err = sd.GetChannels(context.Background())
 		if err != nil {
 			return err
 		}
-	case *flagUsers:
+	case list.users:
 		rep = sd.Users
 	default:
 		return fmt.Errorf("don't know what to do")
 	}
 
 	log.Print("done")
-	switch outputType {
-	case outputTypeJSON:
-		data, err := json.Marshal(rep)
-		if err != nil {
-			return fmt.Errorf("error dumping JSON")
-		}
-		output.Write(data)
+	switch output.format {
 	case outputTypeText:
-		rep.ToText(output)
+		return rep.ToText(w)
+	case outputTypeJSON:
+		enc := json.NewEncoder(w)
+		return enc.Encode(rep)
+	default:
+		return errors.New("invalid output format")
 	}
-
-	return nil
+	// unreachable
 }
 
-func dumpChannels(ctx context.Context, chans []string) (int, error) {
-	var n int
-
-	sd, err := slackdump.New(ctx, *tokenID, *cookie)
+func dumpChannels(ctx context.Context, creds slackCreds, ids []string, dumpfiles bool, generateText bool) (int, error) {
+	sd, err := slackdump.New(ctx, creds.token, creds.cookie)
 	if err != nil {
 		return 0, err
 	}
-	for _, ch := range flag.Args() {
+
+	var total int
+	for _, ch := range ids {
 		log.Printf("dumping channel: %q", ch)
 
-		if err := dumpChannel(ctx, sd, ch); err != nil {
+		if err := dumpOneChannel(ctx, sd, ch, dumpfiles, generateText); err != nil {
 			log.Printf("channel %q: %s", ch, err)
 			continue
 		}
 
-		n++
+		total++
 	}
-	return n, nil
+	return total, nil
 }
 
-func dumpChannel(ctx context.Context, sd *slackdump.SlackDumper, c string) error {
-	var wg sync.WaitGroup
-	f, err := os.Create(c + ".json")
+func dumpOneChannel(ctx context.Context, sd *slackdump.SlackDumper, id string, dumpfiles bool, generateText bool) error {
+	f, err := os.Create(id + ".json")
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	m, err := sd.DumpMessages(ctx, c, *flagDumpFiles)
+	m, err := sd.DumpMessages(ctx, id, dumpfiles)
 	if err != nil {
 		return err
 	}
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "  ")
-	if *outputType == outputTypeText {
-		wg.Add(1)
-		go func() {
-			log.Printf("generating %s.txt", c)
-			t, err := os.Create(c + ".txt")
-			if err != nil {
-				log.Printf("json written ok, but text: %s", err)
-			} else {
-				defer t.Close()
-				m.ToText(t)
-			}
-			wg.Done()
-		}()
-	}
 	if err := enc.Encode(m); err != nil {
 		return err
 	}
+	if generateText {
+		if err := formatTextFile(m, id); err != nil {
+			log.Printf("error creating text file: %s", err)
+		}
+	}
 
-	wg.Wait()
 	return nil
+}
+
+func formatTextFile(m *slackdump.Messages, id string) error {
+	log.Printf("generating %s.txt", id)
+	t, err := os.Create(id + ".txt")
+	if err != nil {
+		return err
+	}
+	defer t.Close()
+
+	return m.ToText(t)
 }
