c53e0cc739463d36721584910a503a9c1b6b8819
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-03-21 22:55:26 +1000

    add test for isExistingFile

--- slackdump_test.go
+++ slackdump_test.go
@@ -3,6 +3,8 @@ package slackdump
 import (
 	"context"
 	"math"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -324,6 +326,32 @@ func Test_newLimiter(t *testing.T) {
 
 			assert.NoError(t, err)
 			assert.WithinDurationf(t, start.Add(tt.wantDelay), stop, 10*time.Millisecond, "delayed for: %s, expected: %s", stop.Sub(start), tt.wantDelay)
+		})
+	}
+}
+
+func Test_isExistingFile(t *testing.T) {
+	testfile := filepath.Join(t.TempDir(), "cookies.txt")
+	if err := os.WriteFile(testfile, []byte("blah"), 0600); err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		cookie string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"not a file", args{"$blah"}, false},
+		{"file", args{testfile}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isExistingFile(tt.args.cookie); got != tt.want {
+				t.Errorf("isExistingFile() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
