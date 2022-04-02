69206388b7418a83f5130e96e39492e223d962f9
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-04-02 22:48:04 +1000

    saveChannel test

--- internal/export/export_test.go
+++ internal/export/export_test.go
@@ -1,15 +1,21 @@
 package export
 
 import (
-	"os/exec"
+	"encoding/json"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/rusq/slackdump"
 	"github.com/rusq/slackdump/internal/fixtures"
 	"github.com/slack-go/slack"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExport_saveChannel(t *testing.T) {
+	//TODO
 	dir := t.TempDir()
 	type fields struct {
 		dir    string
@@ -20,10 +26,11 @@ func TestExport_saveChannel(t *testing.T) {
 		msgs        messagesByDate
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name              string
+		fields            fields
+		args              args
+		wantErr           bool
+		wantMessageByDate messagesByDate
 	}{
 		{
 			"save ok",
@@ -31,12 +38,27 @@ func TestExport_saveChannel(t *testing.T) {
 			args{
 				"unittest",
 				messagesByDate{
+					"2020-12-30": []ExportMessage{
+						{Msg: fixtures.Load[slack.Msg](fixtures.SimpleMessageJSON)},
+					},
 					"2020-12-31": []ExportMessage{
 						{Msg: fixtures.Load[slack.Msg](fixtures.SimpleMessageJSON)},
+						{Msg: fixtures.Load[slack.Msg](fixtures.BotMessageThreadParentJSON)},
+						{Msg: fixtures.Load[slack.Msg](fixtures.BotMessageThreadChildJSON)},
 					},
 				},
 			},
 			false,
+			messagesByDate{
+				"2020-12-30": []ExportMessage{
+					{Msg: fixtures.Load[slack.Msg](fixtures.SimpleMessageJSON)},
+				},
+				"2020-12-31": []ExportMessage{
+					{Msg: fixtures.Load[slack.Msg](fixtures.SimpleMessageJSON)},
+					{Msg: fixtures.Load[slack.Msg](fixtures.BotMessageThreadParentJSON)},
+					{Msg: fixtures.Load[slack.Msg](fixtures.BotMessageThreadChildJSON)},
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -48,12 +70,36 @@ func TestExport_saveChannel(t *testing.T) {
 			if err := se.saveChannel(tt.args.channelName, tt.args.msgs); (err != nil) != tt.wantErr {
 				t.Errorf("Export.saveChannel() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			cmd := exec.Command("ls", "-lR", dir)
-			data, err := cmd.CombinedOutput()
+			mbd, err := loadTestDir(filepath.Join(dir, tt.args.channelName))
 			if err != nil {
 				t.Error(err)
 			}
-			t.Log(string(data))
+			assert.Equal(t, tt.wantMessageByDate, mbd)
 		})
 	}
+}
+
+func loadTestDir(path string) (messagesByDate, error) {
+	// no proper error checking.
+	var mbd = make(messagesByDate, 0)
+	if err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if filepath.Ext(path) != ".json" {
+			return nil
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		var mm []ExportMessage
+		dec := json.NewDecoder(f)
+		if err := dec.Decode(&mm); err != nil {
+			return err
+		}
+		mbd[strings.TrimSuffix(filepath.Base(path), ".json")] = mm
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return mbd, nil
 }
