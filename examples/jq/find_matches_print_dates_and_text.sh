f1686f50a9cba9faa108cc5be70549d4611f628d
Joseph Fitzgerald <joefitz@alumni.princeton.edu>
2022-04-19 20:28:21 -0400

    another jq example
    This example finds messages that have the word "joined" (case insensitive) in the text attribute.  It converts the epoch timestamps to human readable.  And it outputs the timestamp and the text for each matching message.

---
+++ examples/jq/find_matches_print_dates_and_text.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+SAMPLE=../sample.json
+
+cat "${SAMPLE}" | jq '.messages[] | select(( .text != null) and (.text | test("JoInEd";"i"))) | (.ts |= (tonumber|todate)) | .ts, .text' | tr -d '"'
