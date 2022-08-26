67aa8ed26fd1df9f21e192182e980fc900317509
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-08-27 08:33:03 +1000

    add rawoutput

---
+++ tools/rawoutput/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/rusq/slackdump/v2/internal/app"
+)
+
+type params struct {
+	creds     app.SlackCreds
+	output    string
+	workspace string
+}
+
+var args params
+
+func init() {
+	flag.StringVar(&args.creds.Token, "token", os.Getenv("SLACK_TOKEN"), "slack token")
+	flag.StringVar(&args.creds.Cookie, "cookie", os.Getenv("COOKIE"), "slack cookie or path to a file with cookies")
+	flag.StringVar(&args.output, "o", "slackdump_raw.txt", "output file")
+	flag.StringVar(&args.workspace, "w", "", "optional slack workspace name or URL")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [flags] <id_1 id_2 ... id_N>\n", os.Args[0])
+		fmt.Fprintln(flag.CommandLine.Output(), "Where id_* are ids or URLs of slack channels or threads and flags are:")
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		fmt.Fprint(flag.CommandLine.Output(), "Error:  missing ids or channel/thread links\n")
+		return
+	}
+
+	ctx := context.Background()
+	if err := run(ctx, args); err != nil {
+		fmt.Fprintf(flag.CommandLine.Output(), "Error occurred: %s", err)
+		return
+	}
+	log.Println("ok")
+}
+
+func run(ctx context.Context, p params) error {
+	prov, err := app.InitProvider(ctx, app.CacheDir(), p.workspace, p.creds)
+	if err != nil {
+		return err
+	}
+	_ = prov
+
+	return nil
+}
