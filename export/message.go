3f06291f5f9d0cafc5ac7b5d62133a12efc953d2
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-08-31 09:49:25 +1000

    remove omitempty for user struct

--- export/message.go
+++ export/message.go
@@ -17,23 +17,23 @@ type ExportMessage struct {
 
 	// additional fields not defined by the slack library, but present
 	// in slack exports
-	UserTeam        string             `json:"user_team,omitempty"`
-	SourceTeam      string             `json:"source_team,omitempty"`
-	UserProfile     *ExportUserProfile `json:"user_profile,omitempty"`
-	ReplyUsersCount int                `json:"reply_users_count,omitempty"`
-	ReplyUsers      []string           `json:"reply_users,omitempty"`
+	UserTeam        string             `json:"user_team"`
+	SourceTeam      string             `json:"source_team"`
+	UserProfile     *ExportUserProfile `json:"user_profile"`
+	ReplyUsersCount int                `json:"reply_users_count"`
+	ReplyUsers      []string           `json:"reply_users"`
 }
 
 type ExportUserProfile struct {
-	AvatarHash        string `json:"avatar_hash,omitempty"`
-	Image72           string `json:"image_72,omitempty"`
-	FirstName         string `json:"first_name,omitempty"`
-	RealName          string `json:"real_name,omitempty"`
-	DisplayName       string `json:"display_name,omitempty"`
-	Team              string `json:"team,omitempty"`
-	Name              string `json:"name,omitempty"`
-	IsRestricted      bool   `json:"is_restricted,omitempty"`
-	IsUltraRestricted bool   `json:"is_ultra_restricted,omitempty"`
+	AvatarHash        string `json:"avatar_hash"`
+	Image72           string `json:"image_72"`
+	FirstName         string `json:"first_name"`
+	RealName          string `json:"real_name"`
+	DisplayName       string `json:"display_name"`
+	Team              string `json:"team"`
+	Name              string `json:"name"`
+	IsRestricted      bool   `json:"is_restricted"`
+	IsUltraRestricted bool   `json:"is_ultra_restricted"`
 }
 
 func (em ExportMessage) Time() time.Time {
