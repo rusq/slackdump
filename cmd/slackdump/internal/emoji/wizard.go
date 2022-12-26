61dd6eb217d2fcea016eceb51b2a3efc9c5945a6
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2022-12-26 22:26:46 +1000

    emoji wizard update

--- cmd/slackdump/internal/emoji/wizard.go
+++ cmd/slackdump/internal/emoji/wizard.go
@@ -10,19 +10,19 @@ import (
 )
 
 func wizard(ctx context.Context, cmd *base.Command, args []string) error {
-	var base string
+	var baseDir string
 	for {
 		var err error
-		base, err = ui.FileSelector("Enter directory or ZIP file name: ", "Emojis will be saved to this directory or ZIP file")
+		baseDir, err = ui.FileSelector("Enter directory or ZIP file name: ", "Emojis will be saved to this directory or ZIP file")
 		if err != nil {
 			return err
 		}
-		if base != "-" && base != "" {
+		if baseDir != "-" && baseDir != "" {
 			break
 		}
 		fmt.Println("invalid filename")
 	}
-	cfg.BaseLoc = base
+	cfg.BaseLoc = baseDir
 
 	var err error
 	ignoreErrors, err = ui.Confirm("Ignore download errors?", true)
