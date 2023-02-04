rusq/slackdump

commit 78ea432e8ebe2e53bb05413d0b911a3627147a63

move dump back to dump

cmd/slackdump/main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"runtime/trace"
 	"strings"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/joho/godotenv"
 	"github.com/rusq/dlog"
 	"github.com/rusq/tracer"
+	"golang.org/x/term"
 
 	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/apiconfig"
 	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/cfg"
@@ -62,15 +66,28 @@ func main() {
 
 	args := flag.Args()
 	if len(args) < 1 {
-		base.Usage()
-	}
+		if !isInteractive() {
+			base.Usage()
+			// Usage terminates the program.
+		}
 
-	base.CmdName = args[0]
-	if args[0] == "help" {
-		help.Help(os.Stdout, args[1:])
-		return
+		next, err := whatDo(os.Stdout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch next {
+		case choiceExit:
+			return
+		case choiceWizard:
+			args = append(args, "wiz")
+		case choiceHelp:
+			fallthrough
+		default:
+			base.Usage()
+		}
 	}
-	//TODO version
+	base.CmdName = args[0]
+
 BigCmdLoop:
 	for bigCmd := base.Slackdump; ; {
 		for _, cmd := range bigCmd.Commands {
@@ -251,4 +268,38 @@ func loadSecrets(files []string) {
 	for _, f := range files {
 		_ = godotenv.Load(f)
 	}
+}
+
+type choice string
+
+const (
+	choiceUnknown choice = ""
+	choiceHelp    choice = "Print help and exit"
+	choiceWizard  choice = "Run wizard"
+	choiceExit    choice = "Exit"
+)
+
+func whatDo(w io.Writer) (choice, error) {
+	fmt.Println()
+	printVersion()
+	fmt.Println()
+	q := &survey.Select{
+		Message: "What do you want to do?",
+		Options: []string{
+			string(choiceHelp),
+			string(choiceWizard),
+			string(choiceExit),
+		},
+		Default: "Print help and exit",
+	}
+	var ans string
+	if err := survey.AskOne(q, &ans); err != nil {
+		return choiceUnknown, err
+	}
+
+	return choice(ans), nil
+}
+
+func isInteractive() bool {
+	return term.IsTerminal(int(os.Stdout.Fd())) && term.IsTerminal(int(os.Stdin.Fd())) && os.Getenv("TERM") != "dumb"
 }

cmd/slackdump/internal/dump/dump.go
@@ -1,31 +1,178 @@
 package dump
 
 import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"runtime/trace"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/rusq/dlog"
+	"github.com/rusq/slackdump/v2"
+	"github.com/rusq/slackdump/v2/auth"
+	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/cfg"
 	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/golang/base"
-	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/list"
+	"github.com/rusq/slackdump/v2/fsadapter"
+	"github.com/rusq/slackdump/v2/internal/app/config"
+	"github.com/rusq/slackdump/v2/internal/app/nametmpl"
+	"github.com/rusq/slackdump/v2/internal/structures"
+	"github.com/rusq/slackdump/v2/types"
 )
+
+//go:embed assets/list_conversation.md
+var dumpMd string
 
 const codeBlock = "```"
 
 // CmdDump is the dump command.
 var CmdDump = &base.Command{
-	UsageLine: "slackdump dump [flags] <IDs or URLs>",
-	Short:     "dump individual conversations or threads",
-	Long: `
-	# Dump Command
-
-This command is an alias for: 
-` + codeBlock + `
-slackdump list conversation
-` + codeBlock + `
-
-To get extended usage help, run ` + "`slackdump help list conversation`",
+	UsageLine:   "slackdump dump [flags] <IDs or URLs>",
+	Short:       "dump individual conversations or threads",
+	Long:        dumpMd,
 	RequireAuth: true,
 	PrintFlags:  true,
 }
 
 func init() {
-	CmdDump.Run = list.RunDump
-	CmdDump.Wizard = list.WizDump
-	CmdDump.Long = list.HelpDump(CmdDump)
+	CmdDump.Run = RunDump
+	CmdDump.Wizard = WizDump
+	CmdDump.Long = HelpDump(CmdDump)
+}
+
+// ErrNothingToDo is returned if there's no links to dump.
+var ErrNothingToDo = errors.New("no conversations to dump, run \"slackdump help dump\"")
+
+type options struct {
+	Oldest       time.Time // Oldest is the timestamp of the oldest message to fetch.
+	Latest       time.Time // Latest is the timestamp of the newest message to fetch.
+	NameTemplate string    // NameTemplate is the template for the output file name.
+	JSONL        bool      // JSONL should be true if the output should be JSONL instead of JSON.
+}
+
+var opts options
+
+// ptr returns a pointer to the given value.
+func ptr[T any](a T) *T { return &a }
+
+// InitDumpFlagset initializes the flagset for the dump command.
+func InitDumpFlagset(fs *flag.FlagSet) {
+	fs.Var(ptr(config.TimeValue(opts.Oldest)), "from", "timestamp of the oldest message to fetch")
+	fs.Var(ptr(config.TimeValue(opts.Latest)), "to", "timestamp of the newest message to fetch")
+	fs.StringVar(&opts.NameTemplate, "ft", nametmpl.Default, "output file naming template.\n")
+	fs.BoolVar(&opts.JSONL, "jsonl", false, "output JSONL instead of JSON")
+}
+
+// RunDump is the main entry point for the dump command.
+func RunDump(ctx context.Context, cmd *base.Command, args []string) error {
+	if len(args) == 0 {
+		base.SetExitStatus(base.SInvalidParameters)
+		return ErrNothingToDo
+	}
+	// Retrieve the Authentication provider.
+	prov, err := auth.FromContext(ctx)
+	if err != nil {
+		base.SetExitStatus(base.SApplicationError)
+		return err
+	}
+
+	// initialize the list of entities to dump.
+	list, err := structures.NewEntityList(args)
+	if err != nil {
+		base.SetExitStatus(base.SInvalidParameters)
+		return err
+	} else if list.IsEmpty() {
+		base.SetExitStatus(base.SInvalidParameters)
+		return ErrNothingToDo
+	}
+
+	// initialize the file naming template.
+	if opts.NameTemplate == "" {
+		opts.NameTemplate = nametmpl.Default
+	}
+	namer, err := newNamer(opts.NameTemplate, "json")
+	if err != nil {
+		base.SetExitStatus(base.SUserError)
+		return fmt.Errorf("file template error: %w", err)
+	}
+
+	// Initialize the filesystem.
+	fs, err := fsadapter.New(cfg.BaseLoc)
+	if err != nil {
+		base.SetExitStatus(base.SApplicationError)
+		return err
+	}
+	defer fs.Close()
+
+	// Initialize the session.
+	cfg.SlackOptions.Filesystem = fs
+	cfg.SlackOptions.Logger = dlog.FromContext(ctx)
+	sess, err := slackdump.NewWithOptions(ctx, prov, cfg.SlackOptions)
+	if err != nil {
+		base.SetExitStatus(base.SApplicationError)
+		return err
+	}
+
+	// Dump conversations.
+	for _, link := range list.Include {
+		conv, err := sess.Dump(ctx, link, opts.Oldest, opts.Latest)
+		if err != nil {
+			return err
+		}
+
+		if err := save(ctx, fs, namer.Filename(conv), conv); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type namer struct {
+	t   *template.Template
+	ext string
+}
+
+func newNamer(tmpl string, eext string) (namer, error) {
+	t, err := template.New("name").Parse(tmpl)
+	if err != nil {
+		return namer{}, err
+	}
+	return namer{t: t, ext: eext}, nil
+}
+
+// Filename returns the filename for the given conversation.
+func (n namer) Filename(conv *types.Conversation) string {
+	var buf strings.Builder
+	if err := n.t.Execute(&buf, conv); err != nil {
+		panic(err)
+	}
+	return buf.String() + "." + n.ext
+}
+
+func save(ctx context.Context, fs fsadapter.FS, filename string, conv *types.Conversation) error {
+	_, task := trace.NewTask(ctx, "saveData")
+	defer task.End()
+
+	f, err := fs.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return json.NewEncoder(f).Encode(conv)
+}
+
+var helpTmpl = template.Must(template.New("dumphelp").Parse(string(dumpMd)))
+
+// HelpDump returns the help message for the dump command.
+func HelpDump(cmd *base.Command) string {
+	var buf strings.Builder
+	if err := helpTmpl.Execute(&buf, cmd); err != nil {
+		panic(err)
+	}
+	return buf.String()
 }

cmd/slackdump/internal/dump/wizard.go
@@ -1,4 +1,4 @@
-package list
+package dump
 
 import (
 	"context"

cmd/slackdump/internal/list/conversation.go
@@ -1,155 +1,0 @@
-package list
-
-import (
-	"context"
-	_ "embed"
-	"errors"
-	"flag"
-	"fmt"
-	"runtime/trace"
-	"strings"
-	"text/template"
-	"time"
-
-	"github.com/rusq/dlog"
-	"github.com/rusq/slackdump/v2"
-	"github.com/rusq/slackdump/v2/auth"
-	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/cfg"
-	"github.com/rusq/slackdump/v2/cmd/slackdump/internal/golang/base"
-	"github.com/rusq/slackdump/v2/fsadapter"
-	"github.com/rusq/slackdump/v2/internal/app/config"
-	"github.com/rusq/slackdump/v2/internal/app/nametmpl"
-	"github.com/rusq/slackdump/v2/internal/structures"
-)
-
-//go:embed assets/list_conversation.md
-var dumpMd string
-
-// CmdListConversation is the command to list conversations.
-var CmdListConversation = &base.Command{
-	UsageLine:   "slackdump list convo [flags] <conversation list>",
-	Short:       "synonym for 'slackdump dump'",
-	PrintFlags:  true,
-	RequireAuth: true,
-	Long:        "",
-}
-
-// ErrNothingToDo is returned if there's no links to dump.
-var ErrNothingToDo = errors.New("no conversations to dump, run \"slackdump help dump\"")
-
-type options struct {
-	Oldest       time.Time // Oldest is the timestamp of the oldest message to fetch.
-	Latest       time.Time // Latest is the timestamp of the newest message to fetch.
-	NameTemplate string    // NameTemplate is the template for the output file name.
-}
-
-var opts options
-
-// ptr returns a pointer to the given value.
-func ptr[T any](a T) *T { return &a }
-
-func init() {
-	CmdListConversation.Run = RunDump
-	CmdListConversation.Long = HelpDump(CmdListConversation)
-	InitDumpFlagset(&CmdListConversation.Flag)
-}
-
-func InitDumpFlagset(fs *flag.FlagSet) {
-	fs.Var(ptr(config.TimeValue(opts.Oldest)), "from", "timestamp of the oldest message to fetch")
-	fs.Var(ptr(config.TimeValue(opts.Latest)), "to", "timestamp of the newest message to fetch")
-	fs.StringVar(&opts.NameTemplate, "ft", nametmpl.Default, "output file naming template.\n")
-}
-
-// RunDump is the main entry point for the dump command.
-func RunDump(ctx context.Context, cmd *base.Command, args []string) error {
-	if len(args) == 0 {
-		base.SetExitStatus(base.SInvalidParameters)
-		return ErrNothingToDo
-	}
-	// Retrieve the Authentication provider.
-	prov, err := auth.FromContext(ctx)
-	if err != nil {
-		base.SetExitStatus(base.SApplicationError)
-		return err
-	}
-
-	// initialize the list of entities to dump.
-	list, err := structures.NewEntityList(args)
-	if err != nil {
-		base.SetExitStatus(base.SInvalidParameters)
-		return err
-	} else if list.IsEmpty() {
-		base.SetExitStatus(base.SInvalidParameters)
-		return ErrNothingToDo
-	}
-
-	lg := dlog.FromContext(ctx)
-
-	// initialize the file naming template.
-	if opts.NameTemplate == "" {
-		lg.Print("File name template is empty, using the default.")
-		opts.NameTemplate = nametmpl.Default
-	}
-	nameTemplate, err := nametmpl.Compile(opts.NameTemplate)
-	if err != nil {
-		base.SetExitStatus(base.SUserError)
-		return fmt.Errorf("file template error: %w", err)
-	}
-
-	// Initialize the filesystem.
-	if fs, err := fsadapter.New(cfg.BaseLoc); err != nil {
-		base.SetExitStatus(base.SApplicationError)
-		return err
-	} else {
-		cfg.SlackOptions.Filesystem = fs
-		defer fs.Close()
-	}
-
-	// Initialize the session.
-	sess, err := slackdump.NewWithOptions(ctx, prov, cfg.SlackOptions)
-	if err != nil {
-		base.SetExitStatus(base.SApplicationError)
-		return err
-	}
-
-	// Dump conversations.
-	for _, link := range list.Include {
-		if err := dump(ctx, sess, nameTemplate, extmap[listType], opts, link); err != nil {
-			base.SetExitStatus(base.SApplicationError)
-			return err
-		}
-	}
-	return nil
-}
-
-// dump dumps the conversation and saves it to the filesystem.  Filesystem is
-// specified in the global config.
-func dump(ctx context.Context, sess *slackdump.Session, t *template.Template, ext string, opts options, link string) error {
-	ctx, task := trace.NewTask(ctx, "dump")
-	defer task.End()
-
-	conv, err := sess.Dump(ctx, link, opts.Oldest, opts.Latest)
-	if err != nil {
-		return err
-	}
-	var buf strings.Builder
-	if err := t.Execute(&buf, conv); err != nil {
-		return err
-	}
-
-	if err := saveData(ctx, sess, conv, buf.String()+"."+ext, listType); err != nil {
-		return err
-	}
-	return nil
-}
-
-var helpTmpl = template.Must(template.New("dumphelp").Parse(string(dumpMd)))
-
-// HelpDump returns the help message for the dump command.
-func HelpDump(cmd *base.Command) string {
-	var buf strings.Builder
-	if err := helpTmpl.Execute(&buf, cmd); err != nil {
-		panic(err)
-	}
-	return buf.String()
-}

cmd/slackdump/internal/list/list.go
@@ -35,7 +35,6 @@ which is sometimes unreasonably slow.
 	Commands: []*base.Command{
 		CmdListUsers,
 		CmdListChannels,
-		CmdListConversation,
 	},
 }
 
@@ -107,16 +106,20 @@ func saveData(ctx context.Context, sess *slackdump.Session, data any, filename s
 	}
 	defer fs.Close()
 
+	if err := writeData(ctx, fs, filename, data, typ, sess.Users); err != nil {
+		return err
+	}
+	dlog.FromContext(ctx).Printf("Data saved to:  %q\n", filepath.Join(cfg.BaseLoc, filename))
+	return nil
+}
+
+func writeData(ctx context.Context, fs fsadapter.FS, filename string, data any, typ format.Type, users []slack.User) error {
 	f, err := fs.Create(filename)
 	if err != nil {
 		return fmt.Errorf("failed to create file: %w", err)
 	}
 	defer f.Close()
-	if err := fmtPrint(ctx, f, data, typ, sess.Users); err != nil {
-		return err
-	}
-	dlog.FromContext(ctx).Printf("Data saved to:  %q\n", filepath.Join(cfg.BaseLoc, filename))
-	return nil
+	return fmtPrint(ctx, f, data, typ, users)
 }
 
 // fmtPrint prints the given data to the given writer, using the given format.
@@ -138,6 +141,8 @@ func fmtPrint(ctx context.Context, w io.Writer, a any, typ format.Type, u []slac
 		return cvt.Channels(ctx, w, u, val)
 	case types.Users:
 		return cvt.Users(ctx, w, val)
+	case *types.Conversation:
+		return cvt.Conversation(ctx, w, u, val)
 	default:
 		return fmt.Errorf("unsupported data type: %T", a)
 	}

cmd/slackdump/version.go
@@ -21,6 +21,10 @@ And by the way, version is: ` + version + `, commit: ` + commit + `, built on ` 
 }
 
 func versionRun(ctx context.Context, cmd *base.Command, args []string) error {
-	fmt.Printf("%s (commit: %s) built on: %s\n", version, commit, date)
+	printVersion()
 	return nil
+}
+
+func printVersion() {
+	fmt.Printf("Slackdump %s (commit: %s) built on: %s\n", version, commit, date)
 }

commit a3cef58f5e5d0c89080ecb8b54f18ef76c2e85bc

restore help command (regressed in 78ea432)

cmd/slackdump/main.go
@@ -87,6 +87,10 @@ func main() {
 		}
 	}
 	base.CmdName = args[0]
+	if args[0] == "help" {
+		help.Help(os.Stdout, args[1:])
+		return
+	}
 
 BigCmdLoop:
 	for bigCmd := base.Slackdump; ; {