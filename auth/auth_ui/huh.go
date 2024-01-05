159a2b86e99c6dcb1d4eb71728dd57d2a54bc6b5
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2024-01-05 12:23:37 +1000

    update auth from cli-remake

--- auth/auth_ui/huh.go
+++ auth/auth_ui/huh.go
@@ -1,6 +1,8 @@
 package auth_ui
 
 import (
+	"errors"
+	"fmt"
 	"io"
 
 	"github.com/charmbracelet/huh"
@@ -29,8 +31,13 @@ func (*Huh) Stop() {}
 func (*Huh) RequestCreds(w io.Writer, workspace string) (email string, passwd string, err error) {
 	f := huh.NewForm(
 		huh.NewGroup(
-			huh.NewInput().Title("Slack email").Inline(true).Value(&email).Validate(valAND(valEmail, valRequired)),
-			huh.NewInput().Title("Slack password").Inline(true).Value(&passwd).Password(true).Validate(valRequired).Password(true),
+			huh.NewInput().
+				Title("You Slack Login Email").Value(&email).
+				Description(fmt.Sprintf("This is the email that you log into %s with.", workspace)).
+				Validate(valAND(valEmail, valRequired)),
+			huh.NewInput().
+				Title("Password").Value(&passwd).
+				Validate(valRequired).Password(true),
 		),
 	)
 	err = f.Run()
@@ -45,10 +52,53 @@ func (*Huh) RequestLoginType(w io.Writer) (int, error) {
 			huh.NewOption("Google", LoginSSO),
 			huh.NewOption("Apple", LoginSSO),
 			huh.NewOption("Login with Single-Sign-On (SSO)", LoginSSO),
-			huh.NewOption("Other", LoginSSO),
+			huh.NewOption("Other/Manual", LoginSSO),
+			huh.NewOption("------", -1),
+			huh.NewOption("Cancel", LoginCancel),
 		).
 		Value(&loginType).
+		Validate(valSepEaster()).
 		Description("If you are not sure, select 'Other'.").
 		Run()
 	return loginType, err
+}
+
+func valSepEaster() func(v int) error {
+	var phrases = []string{
+		"This is a separator, it does nothing",
+		"Seriously, it does nothing",
+		"Stop clicking on it",
+		"Stop it",
+		"Stop",
+		"Why are you so persistent?",
+		"Fine, you win",
+		"Here's a cookie: 🍪",
+		"🍪",
+		"🍪",
+		"Don't be greedy, you already had three.",
+		"Ok, here's another one: 🍪",
+		"Nothing will happen if you click on it again",
+		"",
+		"",
+		"",
+		"You must have a lot of time on your hands",
+		"Or maybe you're just bored",
+		"Or maybe you're just procrastinating",
+		"Or maybe you're just trying to get a cookie",
+		"These are virtual cookies, you can't eat them, but here's another one: 🍪",
+		"🍪",
+		"You have reached the end of this joke, it will now repeat",
+		"Seriously...",
+		"Ah, shit, here we go again",
+	}
+	var i int
+	return func(v int) error {
+		if v == -1 {
+			// separator selected
+			msg := phrases[i]
+			i = (i + 1) % len(phrases)
+			return errors.New(msg)
+		}
+		return nil
+	}
 }
