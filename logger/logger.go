3cb1506b52f3ff502a4d52ff6910611cb7008e54
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2024-04-07 18:39:21 +1000

    backport logger and network changes

--- logger/logger.go
+++ logger/logger.go
@@ -1,23 +1,57 @@
 package logger
 
 import (
-	"io"
+	"context"
 	"log"
 	"os"
 
 	"github.com/rusq/dlog"
 )
 
+// Interface is the interface for a logger.
 type Interface interface {
 	Debug(...any)
 	Debugf(fmt string, a ...any)
+	Debugln(...any)
 	Print(...any)
 	Printf(fmt string, a ...any)
 	Println(...any)
+	IsDebug() bool
 }
 
+// Default is the default logger.  It logs to stderr and debug logging can be
+// enabled by setting the DEBUG environment variable to 1.  For example:
+//
+//	DEBUG=1 slackdump
 var Default = dlog.New(log.Default().Writer(), "", log.LstdFlags, os.Getenv("DEBUG") == "1")
 
-// note: previously ioutil.Discard which is not deprecated in favord of io.Discard
-// so this is valid only from go1.16
-var Silent = dlog.New(io.Discard, "", log.LstdFlags, false)
+// Silent is a logger that does not log anything.
+var Silent = silent{}
+
+// Silent is a logger that does not log anything.
+type silent struct{}
+
+func (s silent) Debug(...any)                {}
+func (s silent) Debugf(fmt string, a ...any) {}
+func (s silent) Debugln(...any)              {}
+func (s silent) Print(...any)                {}
+func (s silent) Printf(fmt string, a ...any) {}
+func (s silent) Println(...any)              {}
+func (s silent) IsDebug() bool               { return false }
+
+type logCtx uint8
+
+const (
+	logCtxKey logCtx = iota
+)
+
+func NewContext(ctx context.Context, l Interface) context.Context {
+	return context.WithValue(ctx, logCtxKey, l)
+}
+
+func FromContext(ctx context.Context) Interface {
+	if l, ok := ctx.Value(logCtxKey).(Interface); ok {
+		return l
+	}
+	return Default
+}
