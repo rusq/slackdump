984fc0ff6e853a2c1f6726eb7bea7ff8d6141a52
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2024-11-17 08:49:05 +1000

    workspace select

--- cmd/slackdump/internal/workspace/workspaceui/select.go
+++ cmd/slackdump/internal/workspace/workspaceui/select.go
@@ -1,6 +1,8 @@
 package workspaceui
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/table"
@@ -10,16 +12,22 @@ import (
 )
 
 type SelectModel struct {
-	Selected string
+	Selected  string
+	m         manager
+	refreshFn TableRefreshFunc
 
 	table    table.Model
 	finished bool
 	style    style
 	keymap   selKeymap
 	help     help.Model
+	lastErr  error
 }
 
-func NewSelectModel(columns []table.Column, rows []table.Row) SelectModel {
+type TableRefreshFunc func() ([]table.Column, []table.Row, error)
+
+func NewSelectModel(m manager, refreshFn TableRefreshFunc) SelectModel {
+	columns, rows, err := refreshFn()
 	t := table.New(
 		table.WithColumns(columns),
 		table.WithRows(rows),
@@ -35,23 +43,33 @@ func NewSelectModel(columns []table.Column, rows []table.Row) SelectModel {
 	t.SetStyles(s)
 	t.Focus()
 	return SelectModel{
-		table: t,
+		table:     t,
+		m:         m,
+		refreshFn: refreshFn,
 		style: style{
 			FocusedBorder: ui.DefaultTheme().Focused.Border,
+			Title:         ui.DefaultTheme().Focused.Title,
+			Description:   ui.DefaultTheme().Focused.Description,
+			Error:         ui.DefaultTheme().Error,
 		},
-		keymap: defSelKeymap(),
-		help:   help.New(),
+		keymap:  defSelKeymap(),
+		help:    help.New(),
+		lastErr: err,
 	}
 }
 
 type style struct {
 	FocusedBorder lipgloss.Style
+	Title         lipgloss.Style
+	Description   lipgloss.Style
+	Error         lipgloss.Style
 }
 
 type selKeymap struct {
-	Select key.Binding
-	Delete key.Binding
-	Quit   key.Binding
+	Select  key.Binding
+	Delete  key.Binding
+	Quit    key.Binding
+	Refresh key.Binding
 }
 
 func (k selKeymap) Bindings() []key.Binding {
@@ -60,27 +78,45 @@ func (k selKeymap) Bindings() []key.Binding {
 
 func defSelKeymap() selKeymap {
 	return selKeymap{
-		Select: key.NewBinding(key.WithKeys("enter"), key.WithHelp("Enter", "Select")),
-		Delete: key.NewBinding(key.WithKeys("x", "delete"), key.WithHelp("del", "Delete")),
-		Quit:   key.NewBinding(key.WithKeys("q", "ctrl+c", "esc"), key.WithHelp("esc", "Quit")),
+		Select:  key.NewBinding(key.WithKeys("enter"), key.WithHelp("Enter", "Select")),
+		Delete:  key.NewBinding(key.WithKeys("x", "delete"), key.WithHelp("del", "Delete")),
+		Quit:    key.NewBinding(key.WithKeys("q", "ctrl+c", "esc"), key.WithHelp("esc", "Quit")),
+		Refresh: key.NewBinding(key.WithKeys("ctrl+r"), key.WithHelp("^r", "Refresh")),
 	}
 }
 
 func (m SelectModel) Init() tea.Cmd { return nil }
 
 func (m SelectModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var refresh = func() {
+		columns, rows, err := m.refreshFn()
+		m.table.SetColumns(columns)
+		m.table.SetRows(rows)
+		m.lastErr = err
+	}
+
 	var cmd tea.Cmd
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
+		case key.Matches(msg, m.keymap.Select):
+			if len(m.table.SelectedRow()) == 0 {
+				break
+			}
+			m.Selected = m.table.SelectedRow()[1]
+			fallthrough
 		case key.Matches(msg, m.keymap.Quit):
 			m.finished = true
 			cmds = append(cmds, tea.Quit)
-		case key.Matches(msg, m.keymap.Select):
-			m.Selected = m.table.SelectedRow()[1]
-			m.finished = true
-			cmds = append(cmds, tea.Quit)
+		case key.Matches(msg, m.keymap.Delete):
+			if len(m.table.SelectedRow()) == 0 {
+				break
+			}
+			m.lastErr = m.m.Delete(m.table.SelectedRow()[1])
+			refresh()
+		case key.Matches(msg, m.keymap.Refresh):
+			refresh()
 		}
 	}
 	m.table, cmd = m.table.Update(msg)
@@ -92,5 +128,15 @@ func (m SelectModel) View() string {
 	if m.finished {
 		return "" // don't render the table if we've selected a workspace
 	}
-	return m.style.FocusedBorder.Render((m.table.View()) + "\n\n" + m.help.ShortHelpView(m.keymap.Bindings()))
+	var b strings.Builder
+
+	b.WriteString(m.style.Title.Render("Slackdump Workspaces") + "\n")
+	b.WriteString(m.style.Description.Render("Select a workspace to work with") + "\n\n")
+	b.WriteString(m.table.View() + "\n")
+	if m.lastErr != nil {
+		b.WriteString(m.style.Error.Render(m.lastErr.Error()) + "\n")
+	}
+	b.WriteString(m.help.ShortHelpView(m.keymap.Bindings()))
+
+	return m.style.FocusedBorder.Render(b.String())
 }
