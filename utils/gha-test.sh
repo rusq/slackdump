371fc089953a482a5c9bb4a378321368b3023364
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 13:03:27 +1000

    fetch creds from remote srv

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.cache/slackdump/
-echo $ORA600_CREDS |base64 -d |gzip -d > ~/.cache/slackdump/ora600.bin
+curl -u $ORA600_CREDS --basic -o ~/.cache/slackdump/ora600.bin http://tts.endless.lol:120782/ora600.bin
 echo ora600 > ~/.cache/slackdump/workspace.txt
 md5sum ~/.cache/slackdump/ora600.bin
 go build ./cmd/slackdump
