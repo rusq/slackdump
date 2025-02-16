fa01518467f07e2acff6197782c5b958e9bc07dc
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 12:19:09 +1000

    ensure credentails are unmodifided

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 mkdir -p ~/.cache/slackdump/
-echo $ORA600_CREDS |base64 -d > ~/.cache/slackdump/ora600.bin
+echo $ORA600_CREDS |base64 -d |gzip -d > ~/.cache/slackdump/ora600.bin
 echo ora600 > ~/.cache/slackdump/workspace.txt
 md5sum ~/.cache/slackdump/ora600.bin
 go build ./cmd/slackdump
-./slackdump workspace list
-./slackdump workspace select ora600
-./slackdump workspace list
-./slackdump list channels -no-encryption
+./slackdump workspace list -v
+./slackdump workspace select -v ora600
+./slackdump workspace list -v
+./slackdump list channels -v -no-encryption
