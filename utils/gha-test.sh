188dd8ea70f204157cd5e08ef57d8a4b667fbf2a
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 13:34:32 +1000

    trace script

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
+set +x
+if [ -z "${ORA600_CREDS}" ]; then
+  echo "ORA600_CREDS is not set"
+  exit 1
+fi
+
+OUTPUT=~/.cache/slackdump/ora600.bin
+
 mkdir -p ~/.cache/slackdump/
-curl -u $ORA600_CREDS --basic http://tts.endless.lol:12087/ora600.bin >~/.cache/slackdump/ora600.bin 
+curl -o ~/.cache/slackdump/ora600.bin -u "${ORA600_CREDS}" --basic http://tts.endless.lol:12087/ora600.bin 
+
+if [ -s ${OUTPUT} ]; then
+  echo "Downloaded ora600.bin"
+else
+  echo "Failed to download ora600.bin"
+  exit 1
+fi
+
 echo ora600 > ~/.cache/slackdump/workspace.txt
+ls -l ~/.cache/slackdump/
+
 md5sum ~/.cache/slackdump/ora600.bin
+
+
 go build ./cmd/slackdump
 ./slackdump workspace list -v
 ./slackdump workspace select -v ora600
