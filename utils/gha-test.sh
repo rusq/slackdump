ef89b03b8e3d60a897ea785a05a451ddcd4b041c
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 11:36:01 +1000

    no encryption flag

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -4,6 +4,6 @@ echo $ORA600_CREDS > ~/.cache/slackdump/ora600.bin
 echo ora600 > ~/.cache/slackdump/workspace.txt
 go build ./cmd/slackdump
 ./slackdump workspace select ora600
-./slackdump workspace list -a
-./slackdump tools info -auth
+./slackdump workspace list -no-encryption -a
+./slackdump tools info -no-encryption -auth
 ./slackdump list channels -no-encryption
