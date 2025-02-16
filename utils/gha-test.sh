144cb3ba37aeb07b9685f2adf68aca3de5e0f28c
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 12:12:34 +1000

    b64 encode creds

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 mkdir -p ~/.cache/slackdump/
-echo $ORA600_CREDS > ~/.cache/slackdump/ora600.bin
+echo $ORA600_CREDS |base64 -d > ~/.cache/slackdump/ora600.bin
 echo ora600 > ~/.cache/slackdump/workspace.txt
 md5sum ~/.cache/slackdump/ora600.bin
 go build ./cmd/slackdump
+./slackdump workspace list
 ./slackdump workspace select ora600
+./slackdump workspace list
 ./slackdump list channels -no-encryption
