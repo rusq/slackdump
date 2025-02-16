2d8fdeda344e239a3896bd3bc7048c04da3e9eb1
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-16 12:09:58 +1000

    another attempt

--- utils/gha-test.sh
+++ utils/gha-test.sh
@@ -2,8 +2,7 @@
 mkdir -p ~/.cache/slackdump/
 echo $ORA600_CREDS > ~/.cache/slackdump/ora600.bin
 echo ora600 > ~/.cache/slackdump/workspace.txt
+md5sum ~/.cache/slackdump/ora600.bin
 go build ./cmd/slackdump
 ./slackdump workspace select ora600
-./slackdump workspace list -no-encryption -a
-./slackdump tools info -no-encryption -auth
 ./slackdump list channels -no-encryption
