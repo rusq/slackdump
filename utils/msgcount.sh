019d86010c2f069bd0dc21f5811cbacd51e1b976
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-02-17 19:13:24 +1000

    shut up, will you

--- utils/msgcount.sh
+++ utils/msgcount.sh
@@ -3,4 +3,4 @@
 # counts the number of messages in the chunk file
 
 # shellcheck disable=SC2086
-for f in *.json.gz; do echo ${f}: $(gzcat $f | jq '(select(.t==0))| .m | length'); done
+for f in *.json.gz; do echo ${f}: "$(gzcat $f | jq '(select(.t==0))| .m | length')"; done
