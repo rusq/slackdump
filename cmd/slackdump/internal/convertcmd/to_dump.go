9d162f2a4fe9c4fa2c351fcc120931d4c2fc7db6
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-03-13 22:05:53 +1000

    move dump converter to convert pkg

--- cmd/slackdump/internal/convertcmd/to_dump.go
+++ cmd/slackdump/internal/convertcmd/to_dump.go
@@ -5,14 +5,10 @@ import (
 	"errors"
 
 	"github.com/rusq/fsadapter"
-	"github.com/rusq/slack"
 
 	"github.com/rusq/slackdump/v3/cmd/slackdump/internal/cfg"
-	"github.com/rusq/slackdump/v3/internal/chunk"
-	"github.com/rusq/slackdump/v3/internal/chunk/transform"
 	"github.com/rusq/slackdump/v3/internal/convert"
 	"github.com/rusq/slackdump/v3/internal/source"
-	"github.com/rusq/slackdump/v3/internal/structures"
 )
 
 var ErrMeaningless = errors.New("meaningless conversion")
@@ -22,7 +18,9 @@ func toDump(ctx context.Context, srcpath, trgloc string, cflg convertflags) erro
 	if err != nil {
 		return err
 	}
-	if st.Has(source.FDump) {
+	if st == source.FUnknown {
+		return ErrSource
+	} else if st.Has(source.FDump) {
 		return ErrMeaningless
 	}
 	src, err := source.Load(ctx, srcpath)
@@ -39,63 +37,7 @@ func toDump(ctx context.Context, srcpath, trgloc string, cflg convertflags) erro
 
 	filesEnabled := cflg.includeFiles && (st.Has(source.FMattermost))
 
-	fh := &fileHandler{
-		fc: convert.NewFileCopier(src, fsa, src.Files().FilePath, source.DumpFilepath, filesEnabled),
-	}
+	conv := convert.NewToDump(src, fsa, convert.DumpWithIncludeFiles(filesEnabled), convert.DumpWithLogger(cfg.Log))
 
-	conv, err := transform.NewDumpConverter(
-		fsa,
-		src,
-		transform.DumpWithLogger(cfg.Log),
-		transform.DumpWithPipeline(fh.copyFiles),
-	)
-	if err != nil {
-		return err
-	}
-
-	channels, err := src.Channels(ctx)
-	if err != nil {
-		return err
-	}
-	if err := conv.Channels(channels); err != nil {
-		return err
-	}
-	for _, c := range channels {
-		if err := conv.Convert(ctx, chunk.ToFileID(c.ID, "", false)); err != nil {
-			return err
-		}
-	}
-
-	users, err := src.Users(ctx)
-	if err != nil {
-		return err
-	}
-	if err := conv.Users(users); err != nil {
-		return err
-	}
-
-	wi, err := src.WorkspaceInfo(ctx)
-	if err != nil {
-		return err
-	}
-	if err := conv.WorkspaceInfo(wi); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-type fileHandler struct {
-	fc *convert.FileCopier
-}
-
-// copyFiles is a pipeline function that extracts files from messages and
-// calls the file copier.
-func (f *fileHandler) copyFiles(channelID string, _ string, mm []slack.Message) error {
-	for _, m := range mm {
-		if err := f.fc.Copy(structures.ChannelFromID(channelID), &m); err != nil {
-			return err
-		}
-	}
-	return nil
+	return conv.Convert(ctx)
 }
---
+++ internal/convert/dump.go
@@ -0,0 +1,113 @@
+package convert
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/rusq/fsadapter"
+	"github.com/rusq/slack"
+
+	"github.com/rusq/slackdump/v3/internal/chunk"
+	"github.com/rusq/slackdump/v3/internal/chunk/transform"
+	"github.com/rusq/slackdump/v3/internal/source"
+	"github.com/rusq/slackdump/v3/internal/structures"
+)
+
+type DumpConverter struct {
+	src       source.Sourcer
+	fsa       fsadapter.FS
+	lg        *slog.Logger
+	withFiles bool
+}
+
+type DumpOption func(*DumpConverter)
+
+func DumpWithIncludeFiles(b bool) DumpOption {
+	return func(s *DumpConverter) {
+		s.withFiles = b
+	}
+}
+
+func DumpWithLogger(log *slog.Logger) DumpOption {
+	return func(s *DumpConverter) {
+		s.lg = log
+	}
+}
+
+// NewToDump creates a new dump converter.
+func NewToDump(src source.Sourcer, trg fsadapter.FS, opts ...DumpOption) *DumpConverter {
+	std := &DumpConverter{
+		src: src,
+		fsa: trg,
+	}
+	for _, opt := range opts {
+		opt(std)
+	}
+	return std
+}
+
+func (d *DumpConverter) Convert(ctx context.Context) error {
+	tfopts := []transform.DumpOption{
+		transform.DumpWithLogger(d.lg),
+	}
+	if d.withFiles {
+		fh := &fileHandler{
+			fc: NewFileCopier(d.src, d.fsa, d.src.Files().FilePath, source.DumpFilepath, d.withFiles),
+		}
+		tfopts = append(tfopts, transform.DumpWithPipeline(fh.copyFiles))
+	}
+	conv, err := transform.NewDumpConverter(
+		d.fsa,
+		d.src,
+		tfopts...,
+	)
+	if err != nil {
+		return err
+	}
+
+	channels, err := d.src.Channels(ctx)
+	if err != nil {
+		return err
+	}
+	if err := conv.Channels(channels); err != nil {
+		return err
+	}
+	for _, c := range channels {
+		if err := conv.Convert(ctx, chunk.ToFileID(c.ID, "", false)); err != nil {
+			return err
+		}
+	}
+
+	users, err := d.src.Users(ctx)
+	if err != nil {
+		return err
+	}
+	if err := conv.Users(users); err != nil {
+		return err
+	}
+
+	wi, err := d.src.WorkspaceInfo(ctx)
+	if err != nil {
+		return err
+	}
+	if err := conv.WorkspaceInfo(wi); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type fileHandler struct {
+	fc *FileCopier
+}
+
+// copyFiles is a pipeline function that extracts files from messages and
+// calls the file copier.
+func (f *fileHandler) copyFiles(channelID string, _ string, mm []slack.Message) error {
+	for _, m := range mm {
+		if err := f.fc.Copy(structures.ChannelFromID(channelID), &m); err != nil {
+			return err
+		}
+	}
+	return nil
+}
