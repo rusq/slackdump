a994f0a2467cb5b2756c7c7072c2ef992c956e61
Rustam <16064414+rusq@users.noreply.github.com>
2026-02-01 08:16:00 +1000

    take care of double full stop in avatar URIs

--- internal/convert/transform/fileproc/avatar_test.go
+++ internal/convert/transform/fileproc/avatar_test.go
@@ -38,3 +38,66 @@ func Test_avatarPath(t *testing.T) {
 		})
 	}
 }
+
+func TestAvatarProc_removeDoubleDots(t *testing.T) {
+	type fields struct {
+		dl       Downloader
+		filepath func(u *slack.User) string
+	}
+	type args struct {
+		uri string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "good URI",
+			args: args{"https://example.com/facepalm.jpg"},
+			want: "https://example.com/facepalm.jpg",
+		},
+		{
+			name: "double full stop in the URI",
+			args: args{"https://example.com/facepalm..jpg"},
+			want: "https://example.com/facepalm.jpg",
+		},
+		{
+			name: "zero length",
+			args: args{""},
+			want: "",
+		},
+		{
+			name: "just the extension",
+			args: args{".png"},
+			want: ".png",
+		},
+		{
+			name: "extension and double full stop",
+			args: args{"..tiff"},
+			want: ".tiff",
+		},
+		{
+			name: "no extension",
+			args: args{"https://example.com/buriburi"},
+			want: "https://example.com/buriburi",
+		},
+		{
+			name: "non-ascii",
+			args: args{"ぶりぶり..jpg"},
+			want: "ぶりぶり.jpg",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := AvatarProc{
+				dl:       tt.fields.dl,
+				filepath: tt.fields.filepath,
+			}
+			if got := a.removeDoubleDots(tt.args.uri); got != tt.want {
+				t.Errorf("AvatarProc.removeDoubleDots() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
